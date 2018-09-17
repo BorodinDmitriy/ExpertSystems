733c670f1c215b83d4cf301fd923279d75b4b5a4
Pavel <catkiller95@gmail.com>
2018-09-17 23:43:09 +0300

    Новая 8

--- Lab1/Lab1/Lab1/Logic.cs
+++ Lab1/Lab1/Lab1/Logic.cs
@@ -238,15 +238,15 @@ namespace Lab1
 
             //  Eight
             item = new Production();
-            item.AddCondition(new Condition("A1", 0, 3));
-            item.AddCondition(new Condition("B1", 1, 3));
-            item.AddCondition(new Condition("C1", 0, 3));
-            item.AddCondition(new Condition("A2", 0, 3));
-            item.AddCondition(new Condition("B2", 2, 3));
-            item.AddCondition(new Condition("C2", 0, 3));
-            item.AddCondition(new Condition("A3", 0, 3));
-            item.AddCondition(new Condition("B3", 1, 3));
-            item.AddCondition(new Condition("C3", 0, 3));
+            item.AddCondition(new Condition("A1", 0, 2));
+            item.AddCondition(new Condition("B1", 2, 2));
+            item.AddCondition(new Condition("C1", 0, 2));
+            item.AddCondition(new Condition("A2", 0, 2));
+            item.AddCondition(new Condition("B2", 2, 2));
+            item.AddCondition(new Condition("C2", 0, 2));
+            item.AddCondition(new Condition("A3", 0, 2));
+            item.AddCondition(new Condition("B3", 2, 2));
+            item.AddCondition(new Condition("C3", 0, 2));
 
             item.AddFact(new Fact("Number", 8));
 

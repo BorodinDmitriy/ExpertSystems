c89fae117f32ae41dd2fb5c01ef3cdf4e7d2e445
Borodin Dmitriy <demansen@yandex.ru>
2018-09-17 17:08:34 +0300

    перенос в проект для vs2012

---
+++ Lab1/Lab1/Lab1/Fact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class Fact
+    {
+        private string _Name;
+        private int _Value;
+
+        public Fact(string name, int value)
+        {
+            this._Name = name;
+            this._Value = value;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+
+            set
+            {
+                this._Name = value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this._Value;
+            }
+
+            set
+            {
+                this._Value = value;
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/Lab1/Logic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class Logic
+    {
+        public Logic()
+        {
+
+        }
+
+        public bool DirectOutput(List<Fact> goals, List<Fact> facts)
+        {
+            bool result = false;
+
+            List<int> pointer = new List<int>();
+
+            //  П.1 Проверка цели и фактов
+            result = CheckGoals(goals, facts);
+            if (result)
+                return result;
+
+
+
+            return result;
+        }
+
+
+ 
+        private List<Production> _Productions;
+
+        private bool CheckGoals(List<Fact> goals, List<Fact> facts)
+        {
+            bool result = true;
+
+            for (int I = 0; I < goals.Count; I++)
+            {
+                Fact currectGoal = goals[I];
+                Fact fact = facts.Find((item) =>
+                {
+                    return ((item.Name == currectGoal.Name) && (item.Value == currectGoal.Value));
+                });
+
+                if (fact == null)
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}

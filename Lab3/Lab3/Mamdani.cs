1360e3f91f20f9003f7c112de008f4ba6a028cc3
Pavel <catkiller95@gmail.com>
2018-10-23 00:15:48 +0300

    Фазификация для AtackRate, DefenceRate для игрока и противника

--- Lab3/Lab3/Mamdani.cs
+++ Lab3/Lab3/Mamdani.cs
@@ -14,19 +14,37 @@ namespace Lab3
             InitListRules();
         }
 
-        public void Run(HeroInfo player, HeroInfo enemy)
+        public void Run(double HAR, double HDR, double EAR, double EDR)
         {
-            this.Fuzzification(player);
-            this.Fuzzification(enemy);
+            List<double> heroAR = new List<double>();
+            List<double> heroDR = new List<double>();
+            this.Fuzzification(HAR, ref heroAR, HDR, ref heroDR);
+
+            List<double> enemyAR = new List<double>();
+            List<double> enemyDR = new List<double>();
+            this.Fuzzification(EAR, ref enemyAR, EDR, ref enemyDR);
 
             this.Aggregation();
+
+            return;
         }
 
         private List<Rule> Rules;
 
-        private void Fuzzification(HeroInfo user)
+        private void Fuzzification(double AR, ref List<double> fuzzyAR, double DR, ref List<double> fuzzyDR)
         {
+            //  AR => fuzzyAR ['Missing', 'Basic', 'Advance', 'Expert'];
+            fuzzyAR.Add(MissingAR.GetValue(AR));
+            fuzzyAR.Add(BasicAR.GetValue(AR));
+            fuzzyAR.Add(AdvanceAR.GetValue(AR));
+            fuzzyAR.Add(ExpertAR.GetValue(AR));
 
+            //  DR => fuzzyDR ['Missing', 'Basic', 'Advance', 'Expert'];
+            fuzzyDR.Add(MissingDR.GetValue(DR));
+            fuzzyDR.Add(BasicDR.GetValue(DR));
+            fuzzyDR.Add(AdvanceDR.GetValue(DR));
+            fuzzyDR.Add(ExpertDR.GetValue(DR));
+            return;
         }
 
         private void Aggregation()
@@ -34,31 +52,31 @@ namespace Lab3
 
         }
 
+
+        //  "AR" FussySet
+        private TriangleSet MissingAR = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+        private TrapezeSet BasicAR = new TrapezeSet(new Point(0, 0), new Point(1, 1), new Point(30, 1), new Point(32, 0));
+        private TrapezeSet AdvanceAR = new TrapezeSet(new Point(31, 0), new Point(32, 1), new Point(71, 1), new Point(73, 0));
+        private TrapezeSet ExpertAR = new TrapezeSet(new Point(72, 0), new Point(74, 1), new Point(100, 1), new Point(100, 0));
+
+        //  "DR" FuzzySet
+        private TriangleSet MissingDR = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(3, 0));
+        private TrapezeSet BasicDR = new TrapezeSet(new Point(1, 0), new Point(4, 1), new Point(32, 1), new Point(36, 0));
+        private TrapezeSet AdvanceDR = new TrapezeSet(new Point(34, 0), new Point(38, 1), new Point(80, 1), new Point(85, 0));
+        private TrapezeSet ExpertDR = new TrapezeSet(new Point(75, 0), new Point(90, 1), new Point(100, 1), new Point(100, 0));
+
+        //  "D" FuzzySet
+        private TriangleSet No = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+        private TriangleSet Low = new TriangleSet(new Point(0, 0), new Point(2, 1), new Point(15, 0));
+        private TrapezeSet Minor = new TrapezeSet(new Point(10, 0), new Point(17, 1), new Point(38, 1), new Point(50, 0));
+        private TrapezeSet Average = new TrapezeSet(new Point(40, 0), new Point(49, 1), new Point(75, 1), new Point(80, 0));
+        private TrapezeSet High = new TrapezeSet(new Point(78, 0), new Point(89, 1), new Point(100, 1), new Point(100, 0));
         private void InitListRules()
         {
             //  AR - Atack Rate
             //  DR - Defence Rate
             //  D - Damage ["No","Low", "Minor", "Average", High]
             Rule temp = new Rule();
-
-            //  "AR" FussySet
-            TriangleSet MissingAR = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(1, 0));
-            TrapezeSet BasicAR = new TrapezeSet(new Point(0, 0), new Point(1, 1), new Point(30, 1), new Point(32, 0));
-            TrapezeSet AdvanceAR = new TrapezeSet(new Point(31, 0), new Point(32, 1), new Point(71, 1), new Point(73, 0));
-            TrapezeSet ExpertAR = new TrapezeSet(new Point(72, 0), new Point(74, 1), new Point(100, 1), new Point(100, 0));
-
-            //  "DR" FuzzySet
-            TriangleSet MissingDR = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(3, 0));
-            TrapezeSet BasicDR = new TrapezeSet(new Point(1, 0), new Point(4, 1), new Point(32, 1), new Point(36, 0));
-            TrapezeSet AdvanceDR = new TrapezeSet(new Point(34, 0), new Point(38, 1), new Point(80, 1), new Point(85, 0));
-            TrapezeSet ExpertDR = new TrapezeSet(new Point(75, 0), new Point(90, 1), new Point(100, 1), new Point(100, 0));
-
-            //  "D" FuzzySet
-            TriangleSet No = new TriangleSet(new Point(0, 0), new Point(0, 1), new Point(1, 0));
-            TriangleSet Low = new TriangleSet(new Point(0, 0), new Point(2, 1), new Point(15, 0));
-            TrapezeSet Minor = new TrapezeSet(new Point(10, 0), new Point(17, 1), new Point(38, 1), new Point(50, 0));
-            TrapezeSet Average = new TrapezeSet(new Point(40, 0), new Point(49, 1), new Point(75, 1), new Point(80, 0));
-            TrapezeSet High = new TrapezeSet(new Point(78, 0), new Point(89, 1), new Point(100, 1), new Point(100, 0));
 
             //  if ("AR" is "Missing" and "DR" is "Missing") then ("D" is "Low")
             temp.Conditions.Add(new Condition("AR", MissingAR));

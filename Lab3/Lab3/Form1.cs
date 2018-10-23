0a548c95db58da9abb7c186480e3ba0b7a7c1fa0
Pavel <catkiller95@gmail.com>
2018-10-23 03:29:56 +0300

    Просто демонстрашка

--- Lab3/Lab3/Form1.cs
+++ Lab3/Lab3/Form1.cs
@@ -15,6 +15,12 @@ namespace Lab3
         public Form1()
         {
             InitializeComponent();
+            FuzzyController.RunMamdani(fakePlayer, fakeEnemy);
         }
+
+        private Controller FuzzyController = new Controller();
+        private HeroInfo fakePlayer = new HeroInfo(10, 10, 10, 50, 35);
+        private HeroInfo fakeEnemy = new HeroInfo(10, 10, 10, 72.5, 85);
+        
     }
 }
